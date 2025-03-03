bffd6f673a81c1c392bae4effac620618c68d947
simOrder <s1mord3r@gmail.com>
2025-03-03 21:15:30 +0100

    Update settings.py

--- raspiPOS/settings.py
+++ raspiPOS/settings.py
@@ -25,9 +25,6 @@ host_ip = get_ip_address()
 # Add the IP address dynamically to ALLOWED_HOSTS
 ALLOWED_HOSTS = ['localhost', 'simorder.local', host_ip]
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
